fb34ea3102326ff718a82dc0ad0d621a089833b9
alainpham <13981031+alainpham@users.noreply.github.com>
2026-04-09 09:26:22 +0200

    nootel deploy

---
+++ deploy-no-otel.sh
@@ -0,0 +1,8 @@
+kubectl create ns java-apps
+kubectl apply -n java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/hub-deploy.yaml
+kubectl apply -n  java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/availability-calculator-deploy.yaml
+kubectl apply -n  java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/notification-dispatcher-deploy.yaml
+kubectl apply -n  java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/sms-deploy.yaml
+kubectl apply -n  java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/email-deploy.yaml
+
+kubectl apply -n  java-apps -f https://raw.githubusercontent.com/alainpham/business-platform/refs/heads/master/k8s-deployment-no-otel/k6.yaml
